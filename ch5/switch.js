80fd9381cc747a9bb1acc7717bf31113ce79029f
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-09-16 11:58:26 +0900

    feat : 조건문 공부

---
+++ ch5/switch.js
@@ -0,0 +1,13 @@
+var n = 2;
+
+switch(n) {
+     case 1:
+          console.log('1번을 선택');
+          break;
+     case 2:
+          console.log('2번을 선택');
+          break;    
+     case 3:
+          console.log('3번을 선택');
+          break; 
+}
