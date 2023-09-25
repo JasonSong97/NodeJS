82e83cc5134e7cea25ec5feb16fd19d9a36d4a1b
Jason_song97 <jaegeunsong97@gmail.com>
2023-09-26 03:04:53 +0900

    몽고DB /list 요청시 데이터 조회

--- server/Server.js
+++ server/Server.js
@@ -1,49 +1,52 @@
-// Node.js - MongoDB 연동코드
-const mongoclient = require('mongodb').MongoClient;
+// Mongo + NodeJS
+const mongoclient = require("mongodb").MongoClient;
 const url = 'mongodb+srv://admin:1234@cluster0.s8uqhla.mongodb.net/';
-
 let mydb;
 
-mongoclient.connect(url)
-     .then((client) => {
-          mydb = client.db('myboard');
-          mydb.collection('post').find().toArray().then(result => {
-               console.log(result);
-          })
-          
-          app.listen(8080, function() {
-               console.log('포트 8080으로 서버 대기중.........');
-          });
-     }).catch(err => {
-          console.log(err);
+mongoclient.connect(url).then((client) => {
+    mydb = client.db("myboard");
+    // mydb.collection('post').find().toArray().then(result =>{
+    //     console.log(result);
+    // })
+
+    app.listen(8080, function () {
+          console.log("포트 8080으로 서버 대기중 ... ");
+    });
+}).catch((err) => {
+    console.log(err);
 });
 
-// Node.js - MySQL 연동코드
-// var mysql = require('mysql');
+// MySQL + nodeJS
+// var mysql = require("mysql");
 // var conn = mysql.createConnection({
-//      host: 'localhost',
-//      user: 'root',
-//      password: 'abc0701!!',
-//      database: 'myboard'
+//   host: "localhost",
+//   user: "root",
+//   password: "123456",
+//   database: "myboard",
 // });
-
 // conn.connect();
 
-//
-const express = require('express'); // express 라이브러리 사용해서 express 객체 생성
-const app = express(); // express 사용해서 새로운 app 객체 생성, app : 서버 객체라고 생각
+const express = require("express");
+const app = express();
 
-app.get('/book', function(req, res) {
-     res.send('도서 목록 관련 페이지입니다. ');
+// app.listen(8080, function(){
+//     console.log("포트 8080으로 서버 대기중 ... ")
+// });
+
+app.get("/book", function (req, res) {
+     res.send("도서 목록 관련 페이지입니다.");
 });
 
-app.get('/', function(req, res) {
-     res.sendFile(__dirname + '/index.html'); //__dirname : 현재 디렉토리 
+app.get("/", function (req, res) {
+     res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/list', function(req, res) {
-     conn.query("select * from post", function(err, rows, fields) {
-          if (err) throw err;
-          console.log(rows);
+app.get("/list", function (req, res) {
+     // conn.query("select * from post", function (err, rows, fields) {
+     //   if (err) throw err;
+     //   console.log(rows);
+     // });
+     mydb.collection("post").find().toArray().then((result) => {
+          console.log(result);
      });
 });
